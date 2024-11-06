7138ec53d67b83d37fbd5ae1240d4ee9b091e69a
GuilhermeBarboosa <guilherme.strg@gmail.com>
2024-11-06 12:40:10 -0300

    feat responsividade com navbar

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ document.addEventListener("DOMContentLoaded", function () {
     headers.forEach(header => {
         header.addEventListener("click", function () {
 
+            if (header.classList.contains("pb-5")) {
+                header.classList.remove("pb-5");
+                header.classList.add("pb-0");
+            } else {
+                header.classList.remove("pb-0");
+                header.classList.add("pb-5");
+            }
+
             const currentMenu = this.nextElementSibling;
+
 
             if (currentMenu.style.display === "none" || currentMenu.style.display === "") {
                 currentMenu.style.display = "block";
@@ -15,3 +24,23 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector(".container.navbar");
+    const img = document.querySelector(".logo-gov");
+    const placeholder = document.querySelector(".navbar-placeholder");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 0) {
+            navbar.classList.add("fixed-navbar");
+            placeholder.style.display = "block"; // Exibe o placeholder para evitar o pulo
+            img.style.width = "40%";
+        } else {
+            navbar.classList.remove("fixed-navbar");
+            placeholder.style.display = "none"; // Oculta o placeholder
+            img.style.width = "60%";
+        }
+    });
+});
+
